f117ee3d2fb4ffbbe3101ea17f59a232162b20fb
Ramon Borba <ramonborba97@gmail.com>
2023-08-19 03:00:03 -0300

    Assignments: StudentRegistry: Add main menu

--- Assignments/StudentRegistry/main.cpp
+++ Assignments/StudentRegistry/main.cpp
@@ -2,23 +2,73 @@
 
 #include "StudentRegistry.h"
 
+void pressEnterToContinue();
+
 int main(int argc, char const *argv[])
 {
+    using namespace std;
 
     StudentRegistry reg;
 
-    reg.addStudent("Ramon", 10, 10);
-    reg.addStudent("Harry", 7.5, 7);
-    reg.addStudent("Ron", 7, 7);
-    reg.addStudent("Hermionie", 8.5, 9);
-    reg.addStudent("Draco", 7.2, 8);
+    bool exit = false;
+    int command = -1;
+    
+    system("clear");
+    while (!exit)
+    {
+        cout << "----- Student Registry System -----\n\n";
 
-    reg.printRegistry();
+        cout << "Available commands:\n";
+        cout << "   1 - Add student\n";
+        cout << "   2 - Search student\n";
+        cout << "   3 - Update student\n";
+        cout << "   4 - Remove student\n";
+        cout << "   5 - List registry\n\n";
+        cout << "Command: ";
+        cin >> command;
+        cout << endl;
 
-    reg.updateStudent();
-    reg.updateStudent(1, "Harry Potter", 8.3, 8.5);
-
-    reg.printRegistry();
-
+        switch (command)
+        {
+        case 1:
+            reg.addStudent();
+            break;
+        
+        case 2:
+            reg.printStudent();
+            break;
+        
+        case 3:
+            reg.updateStudent();
+            break;
+        
+        case 4:
+            reg.deleteStudent();
+            break;
+        
+        case 5:
+            reg.printRegistry();
+            break;
+        
+        case 0:
+            exit = true;
+            system("clear");
+            continue;
+        
+        default:
+            cout << "Invalid commmand.\n" << endl;
+            break;
+        }
+        pressEnterToContinue();
+    }
+    
     return 0;
+}
+
+void pressEnterToContinue()
+{
+    std::cout << std::endl << "Press 'Enter' to continue.";
+    std::cin.ignore();
+    std::cin.get();
+    system("clear");
 }
